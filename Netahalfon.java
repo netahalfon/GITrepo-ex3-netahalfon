82a4f767a6e36ca6a1cb8db27b276887bcd67164
netahalfon <neta303366@gmail.com>
2025-08-18 13:40:57 +0300

    git commit -m "first commit"

---
+++ Netahalfon.java
@@ -0,0 +1,7 @@
+public class Netahalfon {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Neta halfon " + i+"\n");
+        }
+    }
+}
